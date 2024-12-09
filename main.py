18b975bf56478917e7e9c08b96c1f0673bdca820
Pranay-Builds <contactpranay10@gmail.com>
2024-12-09 16:22:32 +0530

    Error Handling

--- main.py
+++ main.py
@@ -1,3 +1,146 @@
 import sys
 import requests
-from
+from PyQt5.QtWidgets import (QApplication, QWidget, 
+                            QLabel, QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter City Name", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton('Get Weather', self)
+        self.temperatureLabel = QLabel(self)
+        self.emojiLabel = QLabel(self)
+        self.description_label = QLabel(self)
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperatureLabel)
+        vbox.addWidget(self.emojiLabel)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperatureLabel.setAlignment(Qt.AlignCenter)
+        self.emojiLabel.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+        
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperatureLabel.setObjectName("temperatureLabel")
+        self.emojiLabel.setObjectName("emojiLabel")
+        self.description_label.setObjectName("description_label")
+
+        self.setStyleSheet("""
+        QWidget {
+        background-color: rgb(240, 240, 240);               
+        }
+         
+        QLabel, QPushButton {
+            font-family: calibri;
+            color: black;
+        }
+        QLabel#city_label {
+            font-size: 40px;
+            font-weight: bold;
+            font-style: italic;
+        }
+        QLineEdit#city_input {
+            font-size: 30px;
+            font-weight: bold;
+            padding: 2px;
+        }
+        QLabel#temperatureLabel {
+            font-size: 75px;
+        }
+        QLabel#emojiLabel {
+            font-size: 100px;
+            font-family: Segoe UI emoji;
+        }
+        QPushButton#get_weather_button {
+            font-size: 30px;
+            border: 1px solid black;
+            border-radius: 10px;
+            padding: 5px;
+            cursor: pointer;
+            transition: all 0.25s ease;
+        }
+        QPushButton#get_weather_button:hover {
+            background-color: rgba(200, 200, 200, 0.9); 
+            border: 2px solid gray; 
+            border-radius: 20px;
+        }
+        QLabel#description_label {
+            font-size: 55px;
+        }
+        """)
+
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+    def get_weather(self):
+            api_key = '7985e145c423fff7a45ec3abc9496d65'
+            city = self.city_input.text()
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+            
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+
+                if data["cod"] == 200:
+                    self.display_weather(data)
+            except requests.exceptions.HTTPError as http_error:
+                 match response.status_code:
+                      case 400:
+                           self.display_error("Bad Request\n Please Check Your Input")
+                      case 401:
+                           self.display_error("Unauthorised\n Invalid API Key")
+                      case 403:
+                           self.display_error("Forbidden\n Access is denied")
+                      case 404:
+                           self.display_error("Not Found\n City not found")
+                      case 500:
+                           self.display_error("Internal Server Error\n Please try again later")
+                      case 502:
+                           self.display_error("Bad Gateway\n Invalid response from the server")
+                      case 503:
+                           self.display_error("Service Unavailable\nServer is down. Try again later")
+                      case 504:
+                           self.display_error("Gateway Timeout\n No response from the server")
+                      case _:
+                           self.display_error(f"Some Unexpected Error Occured, {http_error}")
+            except requests.exceptions.ConnectionError:
+                 self.display_error("Connection Error:\n Please check your internet connection")
+            except requests.exceptions.Timeout:
+                 self.display_error("Timeout Error:\n The request timed out")
+            except requests.exceptions.TooManyRedirects:
+                 self.display_error('Too many Redirecets:\n Check the URL')
+            except requests.exceptions.RequestException as req_error:
+                 self.display_error(f"Request Error:\n{req_error}")
+            
+            
+            print(data)
+
+
+    def display_error(self):
+            pass
+
+    def display_weather(self, data):
+            print(data)
+
+if __name__ == "__main__":  
+    app = QApplication(sys.argv)
+    weatherApp = WeatherApp()
+    weatherApp.show()
+    sys.exit(app.exec_())
